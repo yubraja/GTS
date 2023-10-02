d160a618e319c3a4867b866afa9e57b8941e5bce
iamsuman <suman.191544@ncit.edu.np>
2023-10-03 00:47:36 +0545

    isDriverActive API is completed from scratch and it is 12:48AM

--- node_backend/src/routes/driverVerification.js
+++ node_backend/src/routes/driverVerification.js
@@ -1,20 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User=require('../models/user')
+const User = require("../models/user");
 
+// Route to list out drivers for verification
+router.get("/listout", async (req, res) => {
+  try {
+    const users = await User.find({
+      role: "Driver",
+      isActualDriver: false,
+    });
 
-//routes for actual Driver verification
-router.get('/:id',async(req,res)=>{
-
-    const user=await User.findById(req.params.id)
-    if(user.isDriverVerified==false){
-        user.isDriverVerified=true
-        await user.save();
-        res.send(user)
+    if (users.length > 0) {
+      res.json(users);
+    } else {
+      res.status(404).json({ message: "No drivers available for verification" });
     }
-    // res.send(false)
-    console.log(user)
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+// Route to update driver verification status
+router.post("/update/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const filter = { _id: userId };
+    const update = { $set: { isActualDriver: true } };
+
+    const updatedUser = await User.findOneAndUpdate(filter, update, {
+      new: true, // Return the updated document
+    });
+
+    if (updatedUser) {
+      res.json(updatedUser);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
